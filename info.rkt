b94823b4e866914d002ae1d7398e5f5eb529e16e
Asumu Takikawa <asumu@ccs.neu.edu>
2012-01-31 21:10:09 -0500

    Initial import (from dfriedman/miniKanren)
    * Renamed to .rkt
    * Use #lang instead of module

---
+++ info.rkt
@@ -0,0 +1,9 @@
+(module info (lib "infotab.ss" "setup")
+  (define name "miniKanren") 
+  (define blurb
+    '("An implementation of miniKanren, an embedding of logic programming into Scheme. This is the miniKanren that was presented at the 2006 Scheme Workshop in September, 2006."))
+  (define categories '(misc))
+  (define can-be-loaded-with 'all)
+  (define doc.txt "doc.txt")
+  (define homepage "http://scheme2006.cs.uchicago.edu/12-byrd.pdf")
+  (define primary-file "minikanren.ss"))
