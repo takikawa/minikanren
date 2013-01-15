c1a9ac9874fd53047fcd4f667b779f2d11c6ae5b
Asumu Takikawa <asumu@ccs.neu.edu>
2013-01-15 00:00:31 -0500

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,5 @@
-(module info (lib "infotab.ss" "setup")
-  (define name "miniKanren") 
-  (define blurb
-    '("An implementation of miniKanren, an embedding of logic programming into Scheme. This is the miniKanren that was presented at the 2006 Scheme Workshop in September, 2006."))
-  (define categories '(misc))
-  (define can-be-loaded-with 'all)
-  (define doc.txt "doc.txt")
-  (define homepage "http://scheme2006.cs.uchicago.edu/12-byrd.pdf")
-  (define primary-file "minikanren.ss"))
+#lang setup/infotab
+
+(define name "minikanren")
+
+(define scribblings '(("minikanren/minikanren.scrbl")))
